ed5f639e166bf0dc8bbeb9b1b86f86ef06e41e97
Aleksey Ivanov <ahtunget@gmail.com>
2018-12-07 02:52:07 +0300

    Convert css to scss. Add bootstrap features.

--- src/app/squads/squads.component.ts
+++ src/app/squads/squads.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-squads',
   templateUrl: './squads.component.html',
-  styleUrls: ['./squads.component.css']
+  styleUrls: ['./squads.component.scss']
 })
 export class SquadsComponent implements OnInit {
 
