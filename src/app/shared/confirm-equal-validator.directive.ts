787c4427d72b2e24d65eb436bb674e59f1f096d9
Aleksey Ivanov <aleksey.ivanov@arcadia.spb.ru>
2019-05-08 18:38:05 +0300

    Добавил кросс-валидацию паролей

--- src/app/shared/confirm-equal-validator.directive.ts
+++
@@ -1,8 +0,0 @@
-import { ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms';
-
-export const PasswordEqualityValidator: ValidatorFn = (fg: FormGroup): ValidationErrors | null => {
-  console.log(fg.get('password').value === fg.get('secondPassword').value);
-  return fg.get('password').value === fg.get('secondPassword').value
-    ? null
-    : { equals: true };
-}
--- src/app/user/personal/forms/basic-info-form/basic-info-form.component.ts
+++ src/app/user/personal/forms/basic-info-form/basic-info-form.component.ts
@@ -1,6 +1,6 @@
+import { PasswordErrorStateMatcher } from './error-state-matcher';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { PasswordEqualityValidator } from 'src/app/shared/confirm-equal-validator.directive';
+import { FormGroup, Validators, FormBuilder, ValidatorFn, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'user-basic-info-form',
@@ -11,6 +11,7 @@ export class BasicInfoFormComponent implements OnInit {
 
   @Output() formReady: EventEmitter<FormGroup> = new EventEmitter();
   mainFormGroup: FormGroup;
+  matcher = new PasswordErrorStateMatcher();
 
   constructor(private _formBuilder: FormBuilder) {
 
@@ -21,9 +22,9 @@ export class BasicInfoFormComponent implements OnInit {
       email: ['', Validators.email],
       passwordFormGroup: this._formBuilder.group({
         password: ['', Validators.minLength(8)],
-        secondPassword: ['', Validators.minLength(8)],
+        secondPassword: [''],
       }, {
-          validator: PasswordEqualityValidator
+          validator: this.passwordEqualityValidator
         }),
       firstName: ['', Validators.pattern(/[а-яА-я]/)],
       lastName: ['', Validators.pattern(/[а-яА-я]/)],
@@ -35,4 +36,11 @@ export class BasicInfoFormComponent implements OnInit {
     this.formReady.emit(this.mainFormGroup);
   }
 
+  passwordEqualityValidator: ValidatorFn = (fg: FormGroup): ValidationErrors | null => {
+    const pass = fg.controls.password.value;
+    const confirmPass = fg.controls.secondPassword.value;
+    return pass === confirmPass ? null : { notEquals: true };
+  }
+
 }
+
